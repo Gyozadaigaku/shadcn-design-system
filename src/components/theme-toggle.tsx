d400c39f2c73586e4a5d1a81969e0d0e84c5ea8d
Gyozadaigaku <gaspardcube15@gmail.com>
2024-03-29 02:54:34 +0900

    ✨ Create `src` dir at the root

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import * as React from "react"
+import { Button } from "@/src/components/ui/button"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
@@ -15,8 +14,8 @@ export function ThemeToggle() {
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
-      <Moon className="hidden h-5 w-5 dark:block" />
+      <Sun className="h-6 w-[1.3rem] dark:hidden" />
+      <Moon className="hidden size-5 dark:block" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
