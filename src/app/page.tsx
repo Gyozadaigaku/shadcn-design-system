904c491cc7d9d47d22142971b1b2a61a64127549
Gyozadaigaku <gaspardcube15@gmail.com>
2024-03-29 11:53:39 +0900

    ♻️ Update files for Storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { buttonVariants } from "@/src/components/ui/button"
+import { buttonVariants } from "@/src/components/ui/Button"
 
 import { siteConfig } from "@/config/site"
 
