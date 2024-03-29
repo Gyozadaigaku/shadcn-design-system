d8676159114c3289849dad7b4afb5b0a2d460265
Gyozadaigaku <gaspardcube15@gmail.com>
2024-03-30 03:33:00 +0900

    ✨ Add template story  component

---
+++ .storybook/_template.stories.tsx
@@ -0,0 +1,39 @@
+/*
+import { action } from "@storybook/addon-actions"
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { XXXXX } from "./XXXXX"
+
+const meta = {
+  title: "Forms/XXXXX",
+  component: XXXXX,
+  tags: ["autodocs"],
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
+} satisfies Meta<typeof XXXXX>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const _XXXXX: Story = {
+  render: () => {
+    const handleClick = action("clicked")
+
+    return (
+      <dl className="m-0 space-y-6 p-4">
+        <div>
+          <dt className="m-0">Heading</dt>
+          <dd className="mx-0 mt-2">
+            <div className="flex flex-row justify-start gap-x-8 gap-y-4">
+              <div className="flex flex-row items-start space-x-2">
+                // Component code goes here
+              </div>
+            </div>
+          </dd>
+        </div>
+      </dl>
+    )
+  },
+}
+*/
