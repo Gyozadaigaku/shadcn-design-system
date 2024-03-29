bfeab10d06ce197979db7694519da2cbb2127017
Gyozadaigaku <gaspardcube15@gmail.com>
2024-03-29 20:44:35 +0900

    ✨ Write stories for Button component

--- src/components/ui/Button.stories.tsx
+++ src/components/ui/Button.stories.tsx
@@ -1,61 +1,210 @@
+import { ChevronRightIcon, EnvelopeOpenIcon } from "@radix-ui/react-icons"
+import { action } from "@storybook/addon-actions"
 import type { Meta, StoryObj } from "@storybook/react"
-import { fn } from "@storybook/test"
 
 import { Button } from "./Button"
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "components/Button",
+  title: "Buttons/Button",
   component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     onClick: { action: "clicked" },
-    variant: {
-      control: {
-        type: "select",
-        options: ["default", "outline", "secondary", "ghost", "link"],
-      },
-    },
-    size: {
-      control: {
-        type: "select",
-        options: ["default", "sm", "lg", "icon"],
-      },
-    },
-    disabled: {
-      control: {
-        type: "boolean",
-      },
-    },
-    children: {
-      control: {
-        type: "text",
-      },
-    },
-    className: {
-      control: {
-        type: "text",
-      },
-    },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
 } satisfies Meta<typeof Button>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-  args: {
-    variant: "default",
-    size: "default",
-    children: "Button",
+export const _Button: Story = {
+  render: () => {
+    const handleClick = action("clicked")
+
+    return (
+      <dl className="m-0 p-4">
+        <dt className="mx-0 mb-4 mt-0">Default</dt>
+        <dd className="mx-0 mb-4 mt-0">
+          <div className="flex flex-col justify-start space-y-4 [&_>_*]:my-0">
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="default" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="default" onClick={handleClick}>
+                <EnvelopeOpenIcon className="mr-2 size-4" />
+                Login with Email
+              </Button>
+              <Button variant="default" onClick={handleClick}>
+                Login with Email
+                <EnvelopeOpenIcon className="ml-2 size-4" />
+              </Button>
+              <Button variant="outline" size="icon" onClick={handleClick}>
+                <ChevronRightIcon className="size-4" />
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button disabled variant="default" onClick={handleClick}>
+                Button
+              </Button>
+              <Button disabled variant="default" onClick={handleClick}>
+                <EnvelopeOpenIcon className="mr-2 size-4" />
+                Login with Email
+              </Button>
+              <Button disabled variant="default" onClick={handleClick}>
+                Login with Email
+                <EnvelopeOpenIcon className="ml-2 size-4" />
+              </Button>
+              <Button
+                disabled
+                variant="outline"
+                size="icon"
+                onClick={handleClick}
+              >
+                <ChevronRightIcon className="size-4" />
+              </Button>
+            </div>
+          </div>
+        </dd>
+
+        <dt className="mx-0 mb-4 mt-0">Variants</dt>
+        <dd className="mx-0 mb-4 mt-0">
+          <div className="flex flex-col justify-start space-y-4 [&_>_*]:my-0">
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="default" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="default" disabled onClick={handleClick}>
+                Button
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="outline" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="outline" disabled onClick={handleClick}>
+                Button
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="secondary" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="secondary" disabled onClick={handleClick}>
+                Button
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="ghost" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="ghost" disabled onClick={handleClick}>
+                Button
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="link" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="link" disabled onClick={handleClick}>
+                Button
+              </Button>
+            </div>
+          </div>
+        </dd>
+
+        <dt className="mx-0 mb-4 mt-0">Small</dt>
+        <dd className="mx-0 mb-4 mt-0">
+          <div className="flex flex-col justify-start space-y-4 [&_>_*]:my-0">
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="default" size="sm" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="default" size="sm" onClick={handleClick}>
+                <EnvelopeOpenIcon className="mr-2 size-4" />
+                Login with Email
+              </Button>
+              <Button variant="default" size="sm" onClick={handleClick}>
+                Login with Email
+                <EnvelopeOpenIcon className="ml-2 size-4" />
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button
+                disabled
+                variant="default"
+                size="sm"
+                onClick={handleClick}
+              >
+                Button
+              </Button>
+              <Button
+                disabled
+                variant="default"
+                size="sm"
+                onClick={handleClick}
+              >
+                <EnvelopeOpenIcon className="mr-2 size-4" />
+                Login with Email
+              </Button>
+              <Button
+                disabled
+                variant="default"
+                size="sm"
+                onClick={handleClick}
+              >
+                Login with Email
+                <EnvelopeOpenIcon className="ml-2 size-4" />
+              </Button>
+            </div>
+          </div>
+        </dd>
+
+        <dt className="mx-0 mb-4 mt-0">Large</dt>
+        <dd className="mx-0 mb-4 mt-0">
+          <div className="flex flex-col justify-start space-y-4 [&_>_*]:my-0">
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button variant="default" size="lg" onClick={handleClick}>
+                Button
+              </Button>
+              <Button variant="default" size="lg" onClick={handleClick}>
+                <EnvelopeOpenIcon className="mr-2 size-4" />
+                Login with Email
+              </Button>
+              <Button variant="default" size="lg" onClick={handleClick}>
+                Login with Email
+                <EnvelopeOpenIcon className="ml-2 size-4" />
+              </Button>
+            </div>
+            <div className="flex flex-wrap gap-2 [&:empty]:gap-0">
+              <Button
+                disabled
+                variant="default"
+                size="lg"
+                onClick={handleClick}
+              >
+                Button
+              </Button>
+              <Button
+                disabled
+                variant="default"
+                size="lg"
+                onClick={handleClick}
+              >
+                <EnvelopeOpenIcon className="mr-2 size-4" />
+                Login with Email
+              </Button>
+              <Button
+                disabled
+                variant="default"
+                size="lg"
+                onClick={handleClick}
+              >
+                Login with Email
+                <EnvelopeOpenIcon className="ml-2 size-4" />
+              </Button>
+            </div>
+          </div>
+        </dd>
+      </dl>
+    )
   },
 }
