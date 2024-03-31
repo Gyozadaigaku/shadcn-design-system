26fd21b834339d32e3c98db2db95346328fbc7c5
Gyozadaigaku <gaspardcube15@gmail.com>
2024-03-31 19:23:13 +0900

    ✨ Write stories for ContextMenu component

---
+++ src/components/ui/ContextMenu/ContextMenu.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "./ContextMenu"
+
+const meta = {
+  title: "Data Display/ContextMenu",
+  component: ContextMenu,
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof ContextMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const _ContextMenu: Story = {
+  render: () => {
+    return (
+      <dl className="m-0 space-y-6 p-4">
+        <div>
+          <dt className="m-0">Default</dt>
+          <dd className="mx-0 mt-2">
+            <div className="flex flex-row justify-start gap-x-8 gap-y-4">
+              <ContextMenu>
+                <ContextMenuTrigger className="flex h-[150px] w-[300px] items-center justify-center rounded-md border border-dashed text-sm">
+                  Right click here
+                </ContextMenuTrigger>
+                <ContextMenuContent className="w-64">
+                  <ContextMenuItem inset>
+                    Back
+                    <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+                  </ContextMenuItem>
+                  <ContextMenuItem inset disabled>
+                    Forward
+                    <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+                  </ContextMenuItem>
+                  <ContextMenuItem inset>
+                    Reload
+                    <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+                  </ContextMenuItem>
+                  <ContextMenuSub>
+                    <ContextMenuSubTrigger inset>
+                      More Tools
+                    </ContextMenuSubTrigger>
+                    <ContextMenuSubContent className="w-48">
+                      <ContextMenuItem>
+                        Save Page As...
+                        <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
+                      </ContextMenuItem>
+                      <ContextMenuItem>Create Shortcut...</ContextMenuItem>
+                      <ContextMenuItem>Name Window...</ContextMenuItem>
+                      <ContextMenuSeparator />
+                      <ContextMenuItem>Developer Tools</ContextMenuItem>
+                    </ContextMenuSubContent>
+                  </ContextMenuSub>
+                  <ContextMenuSeparator />
+                  <ContextMenuCheckboxItem checked>
+                    Show Bookmarks Bar
+                    <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
+                  </ContextMenuCheckboxItem>
+                  <ContextMenuCheckboxItem>
+                    Show Full URLs
+                  </ContextMenuCheckboxItem>
+                  <ContextMenuSeparator />
+                  <ContextMenuRadioGroup value="pedro">
+                    <ContextMenuLabel inset>People</ContextMenuLabel>
+                    <ContextMenuSeparator />
+                    <ContextMenuRadioItem value="pedro">
+                      Pedro Duarte
+                    </ContextMenuRadioItem>
+                    <ContextMenuRadioItem value="colm">
+                      Colm Tuite
+                    </ContextMenuRadioItem>
+                  </ContextMenuRadioGroup>
+                </ContextMenuContent>
+              </ContextMenu>
+            </div>
+          </dd>
+        </div>
+      </dl>
+    )
+  },
+}
